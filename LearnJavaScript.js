2bebdeaf6640a596ab38394e1b77f67127fa5b0a
Ankit <91321175+ankit292@users.noreply.github.com>
2023-11-21 09:49:34 +0000

    array start

--- LearnJavaScript.js
+++ LearnJavaScript.js
@@ -1,1 +1,70 @@
-console.log('Ankit')
+let Name = 'Ankit'
+const lName = 'Kumar'
+var flName = 'Ayush Arya'
+city = 'Delhi'
+let genral;
+//console.table(Name)
+
+let a = '33'
+let b = Number(a)
+//console.table([typeof a,typeof b]);
+
+let aa = 'asa'
+let bb = Boolean(aa)
+//console.table([ aa,  bb]);
+
+// console.log(('1' + '2') + (3 + 4));
+let myObj = [1,2,3]
+// console.log(typeof myObj);
+
+let myBigint = 7772772774664648288n
+// console.log(typeof myBigint);
+
+let att = '        hello          ankit how are you ankit       '
+// let findVal = new String(att)
+// console.log(att.trim())
+
+let number = 10
+let minNum = 5
+//console.log(Number.isFinite(minNum/number));
+
+function intger(x,y){
+    if(Number.isInteger(x/y)){
+        return 'Hello Ankit'
+    }
+    return 'Fuck off'
+}
+//console.log(intger(10,5));
+let parseFlVal = '55.11'
+let convert = Number.parseInt(parseFlVal)
+//console.log(typeof convert);
+function toFix(x){
+    return Number.parseFloat(x).toFixed(1)
+}
+//console.log(toFix(123.456))
+
+let mathNum = 0.55
+//console.log(Math.floor(mathNum));
+
+let minMax = [-1,-2,-3]
+//console.log(Math.min(1,2,3));
+
+let pow1 = 7
+let pow2 = 3
+//console.log(Math.pow(pow1,pow2));
+
+let rand = Math.random()
+let abc = Number.parseInt(Math.floor(rand * 10   )+ 1.55548 )
+//console.log( abc.toFixed(4)   );
+
+let round1 =55
+let round2 = 5
+//console.log(Math.sqrt(round1*round2))
+
+let myDate = new Date()
+//console.log(new Date());
+//console.log(myDate.toUTCString());
+
+//array
+
+
